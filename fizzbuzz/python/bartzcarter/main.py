b5f5eb099bc569d7a22e3528b00983c749e6cf46
bartzcarter <bartzcarter@gmail.com>
2025-02-25 17:33:33 -0700

    new fizzbuzz pr

---
+++ fizzbuzz/python/bartzcarter/main.py
@@ -0,0 +1,28 @@
+import sys
+
+def fizzbuzz(n):
+    for i in range(1,int(n)+1):
+        # Div by both 3 and 5
+        if i % 3 == 0 and i % 5 == 0:
+            print("BobCat")
+        # Div by 5
+        elif i % 5 == 0:
+            print("Cat")
+        # Div by 3
+        elif i % 3 == 0:
+            print("Bob")
+        # Not div by either 3 or 5
+        else:
+            print(i)
+
+def main(args):
+    # Argument passed
+    if len(args) > 1:
+        print(f"Calling fizzbuzz with {args[1]}")
+        fizzbuzz(args[1])
+    # No argument passed
+    else:
+        print("No arguments passed ... calling fizzbuzz() with 15")
+        fizzbuzz(15)
+
+main(sys.argv)
