60406076e6874e3dfb82be302dea1b6f049be54a
EnforcerofLaw5 <157071069+EnforcerofLaw5@users.noreply.github.com>
2025-03-11 19:30:55 -0600

    Updated Fizzbuzz solution

--- Fizzbuzz/Go/braydenmiller/Fizzbuzz.go
+++ Fizzbuzz/Go/braydenmiller/Fizzbuzz.go
@@ -6,19 +6,21 @@ import (
 	"strconv"
 )
 
-func printWords(n int) {
+func generateWords(n int) []string {
+	var result []string
 	for i := 1; i <= n; i++ {
 		switch {
 		case i%15 == 0:
-			fmt.Println("BobCat")
+			result = append(result, "BobCat")
 		case i%3 == 0:
-			fmt.Println("Bob")
+			result = append(result, "Bob")
 		case i%5 == 0:
-			fmt.Println("Cat")
+			result = append(result, "Cat")
 		default:
-			fmt.Println(i)
+			result = append(result, strconv.Itoa(i))
 		}
 	}
+	return result
 }
 
 func main() {
@@ -31,5 +33,9 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	printWords(n)
+
+	words := generateWords(n)
+	for _, word := range words {
+		fmt.Println(word)
+	}
 }
